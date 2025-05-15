7ef63eab5c23b0be8c6ac016a3388d6b5dc919b6
Dhaval523 <dr9373407@gmail.com>
2025-05-15 11:35:48 +0530

    add final

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,19 @@
 import connectDB from './db/db.js'
 import dotenv from 'dotenv'
-import {app, httpServer} from './app.js'
-
-
+import { app, httpServer } from './app.js'
 
 dotenv.config({
-    path:'backend/.env'
+    path: 'backend/.env'
 })
 
 const port = process.env.PORT || 3000
+
 connectDB()
-.then(()=>{
-    httpServer.listen( port ,()=>{
-        console.log("server is running on port" ,port )
+  .then(() => {
+    httpServer.listen(port, '0.0.0.0', () => {
+      console.log("server is running on port", port)
     })
-})
-.catch((error)=>{
-    console.log("connection failed",error)
-})
+  })
+  .catch((error) => {
+    console.log("connection failed", error)
+  })
