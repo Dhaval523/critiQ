c0f60ba96fe51a455104d62389f89c45dc10a323
Dhaval523 <dr9373407@gmail.com>
2025-05-15 13:10:42 +0530

    Update environment variables and CORS configuration to support dynamic frontend URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL || "http://localhost:5173" ,
         credentials: true,
         methods : ["GET", "POST"]
     },
@@ -29,7 +29,7 @@ io.on('connection', (socket) => {
 });
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin:  process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
 }))
 
